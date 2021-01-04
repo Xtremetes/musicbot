7452cdab16341c99aee4f2152c83fc77978201c8
Lucas Ling <lucasling88@gmail.com>
2021-01-05 02:10:20 +0800

    Fixed again

--- index.js
+++ index.js
@@ -16,19 +16,19 @@ const client = new Discord.Client();
 client.commands = new Discord.Collection();
 client.aliases = new Discord.Collection();
 
-// fs.readdir("./commands/", (e, f) => {
-//     if(e) return console.error(e);
-//     f.forEach(file => {
-//         if(!file.endsWith(".js")) return
-//         console.log(`${file} has been loaded`)
-//         let cmd = require(`./commands/${file}`);
-//         let cmdName = cmd.config.name;
-//         client.commands.set(cmdName, cmd)
-//         cmd.config.aliases.forEach(alias => {
-//             client.aliases.set(alias, cmdName);
-//         })
-//     })
-// })
+fs.readdir("./commands/", (e, f) => {
+    if(e) return console.error(e);
+    f.forEach(file => {
+        if(!file.endsWith(".js")) return
+        console.log(`${file} has been loaded`)
+        let cmd = require(`./commands/${file}`);
+        let cmdName = cmd.config.name;
+        client.commands.set(cmdName, cmd)
+        cmd.config.aliases.forEach(alias => {
+            client.aliases.set(alias, cmdName);
+        })
+    })
+})
 
 const queue = new Map();
 
