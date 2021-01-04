6b97a5ce501905c6c5684f48ade1f86a5f5a0c08
Lucas Ling <lucasling88@gmail.com>
2021-01-05 02:50:36 +0800

    LOL

--- commands/queue.js
+++ commands/queue.js
@@ -45,7 +45,7 @@ function embedGenerator(serverQueue){
         let j = i-1;
         const info = current.map(song => `${++j}. [${song.title}](${song.url})`).join('\n')
         const msg = new Discord.MessageEmbed()
-            .setDescription(`Now playing: [${serverQueue.songs[0].title}](${serverQueue.songs[0].url}) \n ${info}`)
+            .setDescription(`**Now playing: [${serverQueue.songs[0].title}](${serverQueue.songs[0].url})** \n \n ${info}`)
         
         embeds.push(msg)
     }
