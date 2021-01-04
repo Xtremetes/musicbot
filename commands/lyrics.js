2c865f41f896e51c17c0920d5d60c30a28564665
Lucas Ling <lucasling88@gmail.com>
2021-01-05 01:31:37 +0800

    Added Queue

--- commands/lyrics.js
+++ commands/lyrics.js
@@ -1,56 +1,57 @@
-const Discord = require('discord.js');
-const lyricFinder = require('lyrics-finder');
+// const Discord = require('discord.js');
+// const lyricFinder = require('lyrics-finder');
 
-module.exports.run = async(client, message, args, queue, searcher) => {
-    if(args.length < 1)
-        return message.lchannel.send("Please enter the artist name first. !lyrics <Artist Name>")
+// module.exports.run = async(client, message, args, queue, searcher) => {
+//     if(args.length < 1)
+//         return message.lchannel.send("Please enter the artist name first. !lyrics <Artist Name>")
 
-    let artist = arg.join(" ");
-    let songName = '';
-    let pages = [];
-    let currentPage = 0;
+//     let artist = arg.join(" ");
+//     let songName = '';
+//     let pages = [];
+//     let currentPage = 0;
 
-    const messageFilter = m => m.author.if === message.author.id;
-    const reactionFilter = (reaction, user) => ['⬅️', '➡️'].includes(reactiom.emoji.name) && (message.author.id === user.id)
+//     const messageFilter = m => m.author.if === message.author.id;
+//     const reactionFilter = (reaction, user) => ['⬅️', '➡️'].includes(reactiom.emoji.name) && (message.author.id === user.id)
 
-    message.channel.send("Please enter the song name now");
-    await message.channel.awaitMessages(messageFilter, { max: 1, time: 15000}).then(async collected => {
-        songName = collected.first().content;
-        await finder (artist, songName, message, pages)
-    })
+//     message.channel.send("Please enter the song name now");
+//     await message.channel.awaitMessages(messageFilter, { max: 1, time: 15000}).then(async collected => {
+//         songName = collected.first().content;
+//         await finder (artist, songName, message, pages)
+//     })
 
-    const lyricEmbed = await message.channel.send(`Lyrics page: ${currentPage+1}/${pages.length}`, pages[currentPage])
-    await lyricEmbed.react('⬅️');
-    await lyricEmbed.react('➡️');
+//     const lyricEmbed = await message.channel.send(`Lyrics page: ${currentPage+1}/${pages.length}`, pages[currentPage])
+//     await lyricEmbed.react('⬅️');
+//     await lyricEmbed.react('➡️');
 
-    const collector = lyricEmbed.createdReactionCollector(reactionFilter);
+//     const collector = lyricEmbed.createdReactionCollector(reactionFilter);
 
-    collector.on('collect', (reaction, user) => {
-        if(reaction.emoji.name === '➡️'){
-            if(currentPage < pages.length-1){
-                currentPage += 1;
-                lyricEmbed.edit(`Lyrics page: ${currentPage+1}/${pages.length}`, pages[currentPage]);
-            }
-            else if(reaction.emoji.name === '⬅️'){
-                currentPage -= 1;
-                lyricEmbed.edit(`Lyrics page: ${currentPage+1}/${pages.length}`, pages[currentPage]);
-            }
-        }
-    })
-}
+//     collector.on('collect', (reaction, user) => {
+//         if(reaction.emoji.name === '➡️'){
+//             if(currentPage < pages.length-1){
+//                 currentPage += 1;
+//                 lyricEmbed.edit(`Lyrics page: ${currentPage+1}/${pages.length}`, pages[currentPage]);
+//                 message.reactions.resolve(reaction).users.remove(user)
+//             }
+//             else if(reaction.emoji.name === '⬅️'){
+//                 currentPage -= 1;
+//                 lyricEmbed.edit(`Lyrics page: ${currentPage+1}/${pages.length}`, pages[currentPage]);
+//             }
+//         }
+//     })
+// }
 
-async function finder(artist, songName, message, pages){
-    let fullLyrics = await lyricsFinder(artist, songName) || "Not Found";
+// async function finder(artist, songName, message, pages){
+//     let fullLyrics = await lyricsFinder(artist, songName) || "Not Found";
 
-    for(let i = 0; i < fullLyrics.length; i += 2048){
-        const lyric = fullLyrics.substring(i, Math.min(fullLyrics.length, i + 2048))
-        const msg = new Discord.MessageEmbed()
-            .setDescription(lyric)
-        pages.push(msg);
-    }
-}
+//     for(let i = 0; i < fullLyrics.length; i += 2048){
+//         const lyric = fullLyrics.substring(i, Math.min(fullLyrics.length, i + 2048))
+//         const msg = new Discord.MessageEmbed()
+//             .setDescription(lyric)
+//         pages.push(msg);
+//     }
+// }
 
-module.exports.config = {
-    name: "lyrics",
-    aliases: [""]
-}
+// module.exports.config = {
+//     name: "lyrics",
+//     aliases: [""]
+// }
--- commands/queue.js
+++ commands/queue.js
@@ -1,18 +1,52 @@
-module.exports.run = (client, message, args, queue, searcher) => {
-    const serverQueue = queue.get(message.guild.id)
+const Discord = require("discord.js");
+
+module.exports.run = async(client, message, args, queue, searcher) => {
+    const serverQueue = queue.get(message.guild.id);
     if(!serverQueue)
         return message.channel.send("There is no music currently playing!");
     if(message.member.voice.channel != message.guild.me.voice.channel)
-        return message.channel.send("You are not in the voice channel");
+        return message.channel.send("You are not in the voice chaqnnel!")
 
-    let nowPlaying = serverQueue.songs[0];
-    let qMsg = `Now playing: ${nowPlaying.title}\n \n`;
+    let currentPage = 0;
 
-    for(var i = 1; i < serverQueue.songs.length; i++){
-        qMsg += `${i}. ${serverQueue.songs[i].title}\n`
+    const embeds = embedGenerator(queue)
+
+    const queueEmbed = await message.channel.send(`Lyrics page: ${currentPage+1}/${embeds.length}`, embeds[currentPage])
+    await queueEmbed.react('⬅️');
+    await queueEmbed.react('➡️');
+
+    const reactionFilter = (reaction, user) => ['⬅️', '➡️'].includes(reactiom.emoji.name) && (message.author.id === user.id)
+    const collector = queueEmbed.createdReactionCollector(reactionFilter);
+
+    collector.on('collect', (reaction, user) => {
+        if(reaction.emoji.name === '➡️'){
+            if(currentPage < embeds.length-1){
+                currentPage += 1;
+                queueEmbed.edit(`Lyrics page: ${currentPage+1}/${embeds.length}`, embeds[currentPage]);
+                message.reactions.resolve(reaction).users.remove(user)
+            }
+            else if(reaction.emoji.name === '⬅️'){
+                currentPage -= 1;
+                queueEmbed.edit(`Lyrics page: ${currentPage+1}/${embeds.length}`, embeds[currentPage]);
+            }
+        }
+    })
+}
+
+function embedGenerator(serverQueue){
+    const embeds = [];
+    let songs = 11;
+    for(let i = 1; i < serverQueue.songs.length; i += 10){
+        const current = serverQueue.songs.slice(i, songs)
+        songs += 10;
+        let j = i-1;
+        const info = current.map(song => `${++j}. [${song.title}](${song.url})`).join('\n')
+        const msg = new Discord.messageEmbed()
+            .setDescription(`Now playing: [${serverQueue.songs[0].title}](${serverQueue.songs[0].url}) \n ${info}`)
+        
+        embeds.push(msg)
     }
-
-    message.channel.send('```' + qMsg + '```');
+    return embeds;
 }
 
 module.exports.config = {
