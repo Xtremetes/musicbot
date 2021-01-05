54badee0bce301d957516e21a78306f7da4c03c8
Lucas Ling <lucasling88@gmail.com>
2021-01-05 17:18:39 +0800

    YES

--- commands/help.js
+++ commands/help.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 
-module.exports.run = (client, message, args, queue, searcher) => {
-    let msg = new Discord.MessageEmbed()
+module.exports.run = (message) => {
+    const msg = new Discord.MessageEmbed()
         .setTitle("Commands")
         .addField("Music")
         .addDescription("```!play``` Plays a song \n```!pause``` Pause the playlist \n```!resume``` Resume the playlist \n```!skip``` Skips the current song \n```!stop``` Stops the playlist entirely \n```loopall``` Loops all the songs in the playlist \n```loopone``` Loops the current song \n```loopoff``` Stops the loop \n```!queue``` Shows the queue")
