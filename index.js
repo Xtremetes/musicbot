6415a1b5885efc5b133d30c1e8f47211ce4a72a5
Lucas Ling <lucasling88@gmail.com>
2021-01-05 17:46:11 +0800

    MSG

--- index.js
+++ index.js
@@ -63,9 +63,9 @@ client.on('message', async message => {
         const msg = new Discord.MessageEmbed()
             .setTitle("Commands")
             .addField("Music")
-            .addDescription("```!play``` Plays a song \n```!pause``` Pause the playlist \n```!resume``` Resume the playlist \n```!skip``` Skips the current song \n```!stop``` Stops the playlist entirely \n```loopall``` Loops all the songs in the playlist \n```loopone``` Loops the current song \n```loopoff``` Stops the loop \n```!queue``` Shows the queue")
+            .setDescription("```!play``` Plays a song \n```!pause``` Pause the playlist \n```!resume``` Resume the playlist \n```!skip``` Skips the current song \n```!stop``` Stops the playlist entirely \n```loopall``` Loops all the songs in the playlist \n```loopone``` Loops the current song \n```loopoff``` Stops the loop \n```!queue``` Shows the queue")
             .addField("Status")
-            .addDescription("```!ping``` Shows the ping of the bot")
+            .setDescription("```!ping``` Shows the ping of the bot")
             .setColor("ORANGE")
         return message.channel.send(msg);
     }
